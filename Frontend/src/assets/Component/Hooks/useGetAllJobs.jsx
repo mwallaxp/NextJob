50b793c89efbe65476e11143551581b1cb21be80
mwallaxp <izmaxp07@gmail.com>
2026-01-22 07:23:52 +0100

    feat:fetching job

--- Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
+++ Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
@@ -10,7 +10,7 @@ const useGetAllJobs = () => {
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
-        const res = await axios.get(`https://nextjob-sw2d.onrender.com/api/v1/job/get/get`, {
+        const res = await axios.get(`https://nextjob-sw2d.onrender.com/api/v1/job/get`, {
           withCredentials: true
         });
         
