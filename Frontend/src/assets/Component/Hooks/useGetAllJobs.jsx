431cee8bc323b903f5b7f7577112683bff8db7d3
mwallaxp <izmaxp07@gmail.com>
2026-01-24 10:29:35 +0100

    improve useGetAllJobs

--- Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
+++ Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
@@ -3,6 +3,7 @@ import { JOB_API_END_POINT } from "../../../utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import {setAllAppliedJobs} from "../../../redux/jobSlice"
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
@@ -11,7 +12,6 @@ const useGetAllJobs = () => {
     const fetchAllJobs = async () => {
       try {
         const token = localStorage.getItem("token");
-        if (!token) return;
 
         const res = await axios.get(`${JOB_API_END_POINT}/get`, {
           headers: {
@@ -23,14 +23,8 @@ const useGetAllJobs = () => {
           dispatch(setAllJobs(res.data.jobs));
         }
       } catch (error) {
-        console.error("Error fetching jobs:", {
-  message: s?.message,
-  response: s?.response?.data,
-  status: s?.response?.status,
-  error: error
-})
-
-        dispatch(setAllJobs([]));
+        console.error("Error fetching jobs:", error);
+        dispatch(setAllAppliedJobs([]));
       }
     };
 
