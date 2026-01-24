d1ce300506f533f4ed97b0a39a357b5aa7c2c7be
mwallaxp <izmaxp07@gmail.com>
2026-01-24 11:02:33 +0100

    improve fetch alljob

--- Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
+++ Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
@@ -1,9 +1,8 @@
-import { setAllJobs } from "../../../redux/jobSlice";
-import { JOB_API_END_POINT } from "../../../utils/constant";
-import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import {setAllAppliedJobs} from "../../../redux/jobSlice"
+import axios from "axios";
+import { setAllJobs, setAllAppliedJobs } from "../../../redux/jobSlice";
+import { JOB_API_END_POINT } from "../../../utils/constant";
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
@@ -13,6 +12,12 @@ const useGetAllJobs = () => {
       try {
         const token = localStorage.getItem("token");
 
+        if (!token) {
+          console.warn("No token found. User might not be logged in.");
+          dispatch(setAllAppliedJobs([]));
+          return;
+        }
+
         const res = await axios.get(`${JOB_API_END_POINT}/get`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -21,9 +26,11 @@ const useGetAllJobs = () => {
 
         if (res.data.success) {
           dispatch(setAllJobs(res.data.jobs));
+        } else {
+          dispatch(setAllAppliedJobs([]));
         }
       } catch (error) {
-        console.error("Error fetching jobs:", error);
+        console.error("Error fetching jobs:", error.response?.data || error.message);
         dispatch(setAllAppliedJobs([]));
       }
     };
