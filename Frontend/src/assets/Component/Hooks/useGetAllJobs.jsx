ad90caca2b7d726df43458e996bdefc465406d0f
mwallaxp <izmaxp07@gmail.com>
2026-01-24 09:19:37 +0100

    feat: redesign useGetAllJob

--- Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
+++ Frontend/src/assets/Component/Hooks/useGetAllJobs.jsx
@@ -4,27 +4,32 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {setAllAppliedJobs} from "../../../redux/jobSlice"
+
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-  const fetchAllJobs = async () => {
-    try {
-      const res = await axios.get(`${JOB_API_END_POINT}/get`, {
-        withCredentials: true,
-      });
+    const fetchAllJobs = async () => {
+      try {
+        const token = localStorage.getItem("token");
 
-      if (res.data.success) {
-        dispatch(setAllJobs(res.data.jobs));
+        const res = await axios.get(`${JOB_API_END_POINT}/get`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (res.data.success) {
+          dispatch(setAllJobs(res.data.jobs));
+        }
+      } catch (error) {
+        console.error("Error fetching jobs:", error);
+        dispatch(setAllAppliedJobs([]));
       }
-    } catch (error) {
-      console.error("Error fetching jobs:", error);
-      dispatch(setAllAppliedJobs([]));
-    }
-  };
+    };
 
-  fetchAllJobs();
-}, []);
+    fetchAllJobs();
+  }, [dispatch]);
 };
 
 export default useGetAllJobs;
