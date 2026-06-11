31484897bd3aa434ecd8516632a177f9de6f0318
mwallaxp <izmaxp07@gmail.com>
2026-06-11 13:52:48 +0100

    solve auth

--- Frontend/src/assets/Component/shared/Layout.jsx
+++ Frontend/src/assets/Component/shared/Layout.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import { Footer } from "../Footer";
+import { AuthProvider } from "./AuthContext"; // Import AuthProvider
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-slate-50">
-      <NavBar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
+      <AuthProvider> {/* Wrap the main content with AuthProvider */}
+        <NavBar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvider>
     </div>
   );
 };
